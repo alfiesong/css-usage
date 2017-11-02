8f5e7de5ac9de266b1cfbc27580f7e5e60616e89
Alfie Song <alson@microsoft.com>
2017-11-02 14:22:12 -0700

    add passive event listener

---
+++ src/recipes/recipe-passiveeventlistener.js
@@ -0,0 +1,40 @@
+/*
+    RECIPE: Passive Event Listener
+    -------------------------------------------------------------
+    Author: alson
+    Description: This counts any page that includes any script referecnces to passive event listener
+    Scenraios:
+    1. addEventListener(any, function, {passive:true}); -> /addEventListener\s*\(\s*\S*,\s*\S*,\s*\{\s*passive\s*:\s*\S*\s*\}\s*\)/g
+    2. addEventListener(any, function, {passive:true, capture:true, once: true});
+    3. addEventListener(any, function, option); var option = { passive: true }; this scenario is hard to have a perfect solution to detect
+    4. addEventListener(any, function, passiveSupported ? { passive: true } : false);
+    Also need to exclude this scenario:
+    1. addEventListener(any, function, true)  this trade "true" as useCapture 
+
+    Since there are so many different scenarios, here we just detect 
+    a. if addEventListener is using a third parameter
+    b. if "passive:" shows up in JS. This indicates a high chance that JS is at least trying to use passive event listener
+
+
+*/
+
+void function() {
+    window.CSSUsage.StyleWalker.recipesToRun.push( function passiveEL( element, results) {
+
+        if (element.nodeName == "SCRIPT") {
+
+            var matchCount = (element.innerText.match(/addEventListener\s*\(\s*\S*,\s*\S*,/g) || []).length;
+            if (element.innerText.indexOf("passive:") != -1 && matchCount > 0) {
+                results["use"] = results["use"] || { count: 0 };
+                results["use"].count++;
+            }
+
+
+
+
+        }
+
+        return results;
+    });
+}();
+
